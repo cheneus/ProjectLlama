89f36a7e0e0bf1b8accf9aeacfe2f863e94c7472
Chen Sia <cheneus@gmail.com>
2018-04-09 14:06:28 -0500

    footer change

--- client/src/components/bootstrap/Footer/Footer.js
+++ client/src/components/bootstrap/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
 <div className="footer">
 	<p className="title">TraveLlama</p>
 	<p>Addison Albrecht, Nicole Cloyd, Yaeinn Park, Chen Sia</p>
-	<p><a href="https://github.com/cheneus/ProjectRed" target="blank">Github Repository</a></p>
+	<p><a href="https://github.com/cheneus/ProjectLlama" target="blank">Github Repository</a></p>
 	<p>&copy; 2018</p>
 </div>
 );
