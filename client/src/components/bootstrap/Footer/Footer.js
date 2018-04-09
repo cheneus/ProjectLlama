aa866f6388a91976956295b750f26ceeebb0ef6c
Chen Sia <cheneus@gmail.com>
2018-04-09 13:55:17 -0500

    spelling error

--- client/src/components/bootstrap/Footer/Footer.js
+++ client/src/components/bootstrap/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => (
 <div className="footer">
 	<p className="title">TraveLlama</p>
-	<p>Addison Albrecht, Nicole Cloyd, Yaeinn Park, Cheneus Sai</p>
+	<p>Addison Albrecht, Nicole Cloyd, Yaeinn Park, Chen Sia</p>
 	<p><a href="https://github.com/cheneus/ProjectRed" target="blank">Github Repository</a></p>
 	<p>&copy; 2018</p>
 </div>
