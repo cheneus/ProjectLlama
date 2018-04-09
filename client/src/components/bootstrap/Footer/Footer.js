79628bfe00b0850c3dd007eb21ede0be826d240f
Chen Sia <cheneus@gmail.com>
2018-04-09 14:14:55 -0500

    footer update

--- client/src/components/bootstrap/Footer/Footer.js
+++ client/src/components/bootstrap/Footer/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.css";
 const Footer = () => (
 <div className="footer">
 	<p className="title">TraveLlama</p>
-	<p>Addison Albrecht, Nicole Cloyd, Yaeinn Park, Cheneus Sai</p>
-	<p><a href="https://github.com/cheneus/ProjectRed" target="blank">Github Repository</a></p>
+	<p>Addison Albrecht, Nicole Cloyd, Yaeinn Park, Chen Sai</p>
+	<p><a href="https://github.com/cheneus/ProjectLlama" target="blank">Github Repository</a></p>
 	<p>&copy; 2018</p>
 </div>
 );
