4e6f38d4687ba2b6c96c096aae14993e28c33ffc
Chen Sia <cheneus@gmail.com>
2018-05-24 23:16:59 -0500

    comment out payload/sign validation for now

--- server/route/authRouter.js
+++ server/route/authRouter.js
@@ -7,48 +7,48 @@ const passport = require('../passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-const validateSignupForm = (payload) => {
-  const errors = {};
-  let isFormValid = true;
-  let message = '';
+// const validateSignupForm = (payload) => {
+//   const errors = {};
+//   let isFormValid = true;
+//   let message = '';
 
-  if (
-    !payload ||
-    typeof payload.email !== 'string' ||
-    !validator.isEmail(payload.email)
-  ) {
-    isFormValid = false;
-    errors.email = 'Please provide a correct email address.';
-  }
+//   if (
+//     !payload ||
+//     typeof payload.email !== 'string' ||
+//     !validator.isEmail(payload.email)
+//   ) {
+//     isFormValid = false;
+//     errors.email = 'Please provide a correct email address.';
+//   }
 
-  if (
-    !payload ||
-    typeof payload.password !== 'string' ||
-    payload.password.trim().length < 8
-  ) {
-    isFormValid = false;
-    errors.password = 'Password must have at least 8 characters.';
-  }
+//   if (
+//     !payload ||
+//     typeof payload.password !== 'string' ||
+//     payload.password.trim().length < 8
+//   ) {
+//     isFormValid = false;
+//     errors.password = 'Password must have at least 8 characters.';
+//   }
 
-  if (
-    !payload ||
-    typeof payload.name !== 'string' ||
-    payload.name.trim().length === 0
-  ) {
-    isFormValid = false;
-    errors.name = 'Please provide your name.';
-  }
+//   if (
+//     !payload ||
+//     typeof payload.name !== 'string' ||
+//     payload.name.trim().length === 0
+//   ) {
+//     isFormValid = false;
+//     errors.name = 'Please provide your name.';
+//   }
 
-  if (!isFormValid) {
-    message = 'Check the form for errors.';
-  }
+//   if (!isFormValid) {
+//     message = 'Check the form for errors.';
+//   }
 
-  return {
-    success: isFormValid,
-    message,
-    errors,
-  };
-};
+//   return {
+//     success: isFormValid,
+//     message,
+//     errors,
+//   };
+// };
 
 router.get('/google',
   passport.authenticate('google', {
